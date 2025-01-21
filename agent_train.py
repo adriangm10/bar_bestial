4a56c779b144c7c1030352ccfa7e3ee2638a6052
adrian <adrianciro@hotmail.es>
2025-01-21 15:17:04 +0100

    test trained model vs random actions

--- agent_train.py
+++ agent_train.py
@@ -1,11 +1,10 @@
+import random
+
 from stable_baselines3 import DQN
 from stable_baselines3.common.callbacks import BaseCallback, EvalCallback
 from stable_baselines3.common.evaluation import evaluate_policy
 
 from bar_gym import BarEnv
-
-env = BarEnv(game_mode="basic")
-
 
 # class CustomCallback(BaseCallback):
 #     def __init__(self, verbose: int = 0):
@@ -54,11 +53,42 @@ env = BarEnv(game_mode="basic")
 #         pass
 
 
-model = DQN("MlpPolicy", env, verbose=1)
+def model_v_random(model, env, num_games=100):
+    obs, _ = env.reset()
+    wins = 0
+    losses = 0
+    draws = 0
+
+    for _ in range(num_games):
+        while True:
+            if env.game.turn == 0:
+                action = model.predict(obs)[0]
+            else:
+                action = random.randint(0, len(env.game.hands[env.game.turn]) - 1)
+
+            obs, r, terminated, truncated, _ = env.step(action)
+            if terminated or truncated:
+                if r > 0:
+                    wins += 1
+                elif r == 0:
+                    draws += 1
+                else:
+                    losses += 1
+                break
+
+    return wins, losses, draws
+
+
+env = BarEnv(game_mode="basic")
+model = DQN("MlpPolicy", env)
 eval_callback = EvalCallback(env, best_model_save_path="models/dqn", eval_freq=5000, n_eval_episodes=1000)
-model.learn(total_timesteps=50000, callback=eval_callback)
+model.learn(total_timesteps=100_000, callback=eval_callback)
 
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=200, deterministic=True)
 print(f"mean_reward: {mean_reward}, std_reward: {std_reward}")
 
 model.save("models/dqn_final")
+
+model = DQN.load("models/dqn_final")
+wins, losses, draws = model_v_random(model, BarEnv(game_mode="basic"))
+print(f"wins: {wins}, losses: {losses}, draws: {draws}")
