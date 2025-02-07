78afabca088fc41cc9a82374128e5293b8538485
adrian <adrianciro@hotmail.es>
2025-02-07 17:11:09 +0100

    add possibility of adding previous turns to the state

--- bar_gym.py
+++ bar_gym.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 
 
 class BarEnv(gym.Env):
+    """ """
+
     metadata = {"render_modes": ["human"], "render_fps": 4}
 
     def __init__(
@@ -20,6 +22,7 @@ class BarEnv(gym.Env):
         game_mode: Literal["basic", "medium", "full"] = "full",
         self_play: bool = False,
         render_mode=None,
+        t: int = 2,
     ):
         if game_mode != "basic":
             raise NotImplementedError
@@ -30,18 +33,25 @@ class BarEnv(gym.Env):
         self.agent_color = Color(0)
         self.opponent_model = opponent_model
         self.self_play = self_play
-        self.agent_heaven = 0
+        # self.agent_heaven = 0
+        # self.opponent_heaven = 0
 
+        self.L = 1  # hand (1)
+        self.M = 6  # heaven (1), hell (1), and queue (4)
+        self.MM = num_players * 6
         self.observation_space = spaces.Box(
             low=0,
             high=1,
             shape=(
-                num_players * 2 + num_players * 4 + 1,  # heaven + hell, board, hand
+                self.MM * t + self.L,  # (heaven + hell, board) * t, hand, color
                 len(CardType.toList()),
             ),
             dtype=np.int32,
         )
         self.action_space = spaces.Discrete(4)
+
+        self.t = t
+        self.history: list[int] = []
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -50,27 +60,32 @@ class BarEnv(gym.Env):
         cardt_count = len(CardType.toList())
         cards_rep = np.zeros(self.observation_space.shape, dtype=np.int32)
 
+        # hand row
+        for c in self.game.hands[self.game.turn]:
+            cards_rep[0][c.value - 1] = 1
+
+        # color row
+        # for i in range(cardt_count):
+        #     cards_rep[1][i] = self.game.turn
+
         # num_players rows for heaven
         for c in self.game.heaven:
-            cards_rep[(c.color.value - self.game.turn) % self.num_players][c.value - 1] = 1
+            cards_rep[((c.color.value - self.game.turn) % self.num_players) * self.M + self.L][c.value - 1] = 1
 
         # num_players rows for hell
         for c in self.game.hell:
-            cards_rep[self.num_players + (c.color.value - self.game.turn) % self.num_players][c.value - 1] = 1
+            cards_rep[((c.color.value - self.game.turn) % self.num_players) * self.M + self.L + 1][c.value - 1] = 1
 
         # 4 * num_players rows for the board, there will never be 5 cards in the queue
         for i, c in enumerate(self.game.table_cards):
             if c is None:
                 break
-            cards_rep[(2 + i) * self.num_players + (c.color.value - self.game.turn) % self.num_players][c.value - 1] = 1
+            cards_rep[((c.color.value - self.game.turn) % self.num_players) * self.M + self.L + 2 + i][c.value - 1] = 1
 
-        # hand row
-        for c in self.game.hands[self.game.turn]:
-            cards_rep[-1][c.value - 1] = 1
+        for i in range(1, min(self.t, len(self.history))):
+            cards_rep[self.L + i * self.MM : self.L + (i + 1) * self.MM] = self.history[-i]
 
-        # color row
-        # for i in range(cardt_count):
-        #     cards_rep[-1][i] = self.game.turn
+        self.history.append(cards_rep[self.L : self.L + self.MM])
 
         return cards_rep
 
@@ -87,9 +102,11 @@ class BarEnv(gym.Env):
     def reset(self, seed: int | None = None, options: dict | None = None):
         super().reset(seed=seed, options=options)
 
-        del self.game, self.agent_color
+        del self.game, self.agent_color, self.history
         self.game = Game(self.num_players, self.game_mode)
         self.agent_color = Color(np.random.randint(0, self.num_players))
+        self.history = []
+        # self.agent_heaven = 0
 
         while self.self_play and self.game.turn != self.agent_color.value:
             act = self._predict_opp()
@@ -110,10 +127,9 @@ class BarEnv(gym.Env):
         hand_count = len(self.game.hands[self.game.turn])
         reward = 0
         if action > hand_count - 1 and self.game.turn == self.agent_color.value:
-            # reward = -0.1
-            logger.info(f"the agent selected card {action + 1} but there are only {hand_count} cards, selecting a card randomly...")
+            reward = -0.1
+            logger.debug(f"the agent selected card {action + 1} but there are only {hand_count} cards, selecting a card randomly...")
             action = np.random.randint(0, hand_count)
-            # return self.obs, -1, True, True, {}
 
         self.game.play_card(action, [])
 
@@ -122,14 +138,20 @@ class BarEnv(gym.Env):
             self.game.play_card(act, [])
             self.obs = self._get_obs()
 
+        # agent_heaven = len([c for c in self.game.heaven if c.color == self.agent_color])
+        # if agent_heaven > self.agent_heaven:
+        #     reward += agent_heaven - self.agent_heaven
+        #     self.agent_heaven = agent_heaven
+
+        # done = self.game.finished()
         if done := self.game.finished():
             winners = self.game.winners()
             if len(winners) == self.num_players or not winners:
-                reward = 0
+                reward += 0
             elif self.agent_color in winners:
-                reward = 1
+                reward += 1
             else:
-                reward = -1
+                reward += -1
 
         self.obs = self._get_obs()
 
